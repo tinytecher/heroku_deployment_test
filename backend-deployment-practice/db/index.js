506f14287aee2740fdf425f536b7827e3679cd85
Charlotte Forte <79761202+tinytecher@users.noreply.github.com>
2022-07-05 11:20:03 +0100

    Delete backend-deployment-practice directory

--- backend-deployment-practice/db/index.js
+++
@@ -1,9 +0,0 @@
-import pg from "pg";
-import { db } from "../config/index.js";
-
-export const pool = new pg.Pool({
-  connectionString: db.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
