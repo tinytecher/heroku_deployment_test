0f80a784aac06a4fecc5943b1909b552fd216cea
School Of Code Staff <staff@YVWFYF3CF0.local>
2022-07-05 16:22:06 +0100

    Revert "test"
    This reverts commit f78f67b29627d71b02dd796b610fb1f308bc6074.

--- bin/www.js
+++ bin/www.js
@@ -14,7 +14,7 @@ const debug = debugLib("helper-videos-backend-deployment:server");
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || "5001");
+const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
 /**
